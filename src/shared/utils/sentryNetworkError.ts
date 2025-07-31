7e628b69e8b0bcd03f40bb436f9a700b8e0f7bcf
yura <yuraa0302@gmail.com>
2025-07-31 21:13:15 +0900

    chore : Sentry 에러 커스터마이징

---
+++ src/shared/utils/sentryNetworkError.ts
@@ -0,0 +1,23 @@
+export class SentryNetworkError extends Error {
+  status: number;
+  url: string;
+
+  constructor(response: Response) {
+    const name = SentryNetworkError.generateName(response);
+    super(name);
+    this.name = name;
+    this.status = response.status;
+    this.url = response.url;
+  }
+
+  private static generateName(response: Response): string {
+    const status = response.status;
+    const fullUrl = response.url;
+    const urlObj = new URL(fullUrl);
+    const baseURL = `${urlObj.origin}`;
+    const path = urlObj.pathname;
+    const replacePathParams = path.replace(/\/\d+(?=\/|$)/g, "/{id}");
+
+    return `[${status} Error] - ${baseURL}${replacePathParams}`;
+  }
+}
