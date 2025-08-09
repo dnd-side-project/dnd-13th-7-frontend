4648611f3aecfa46b31d5a227ed8abf8ac84eb3d
yura <yuraa0302@gmail.com>
2025-08-09 19:26:01 +0900

    style: tailwind.config.ts 수정

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import { THEMES } from './src/styles/colors'
+
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    screens: {},
+    extend: {
+      colors: {
+        ...THEMES,
+      },
+      fontFamily: {
+        pretendard: ['Pretendard'],
+      },
+    },
+  },
+  plugins: [],
+}
