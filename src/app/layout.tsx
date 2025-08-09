94669b4aec207ca49e7e57472a985b1a1124edea
oaoong <pizzachicken@kakao.com>
2025-08-09 23:21:37 +0900

    style: tailwindv4에 기반한 스타일 정의 조정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { GoogleAnalytics } from '@next/third-parties/google'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
-import './globals.css'
+import '@/styles/globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
