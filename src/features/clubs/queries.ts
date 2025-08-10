5e2dc53403946ebb39c9daaed155d4849c9a1f0a
Jeong Jaehee <pizzachicken@kakao.com>
2025-08-10 16:13:26 +0900

    chore: 서버 요청 환경 구성 (#22)
    * chore: tanstack query 및 axios 추가
    
    * fix: 불필요한 폰트 로드 제거
    
    * chore: 기초 tanstack query 사용 환경 구성
    
    * fix: recruit 관련 타입 추가
    
    * fix: axios export 중복 제거

---
+++ src/features/clubs/queries.ts
@@ -0,0 +1,23 @@
+import { useQuery, UseQueryResult } from '@tanstack/react-query'
+import { clubQueries } from './queries.factory'
+import { ClubDetailsData, ClubsPage, ClubRecruitsData } from './types'
+
+export function useClubsList(params?: {
+  page?: number
+  size?: number
+  search?: string
+}): UseQueryResult<ClubsPage, Error> {
+  return useQuery(clubQueries.list(params))
+}
+
+export function useClubDetails(
+  clubId: number | string,
+): UseQueryResult<ClubDetailsData, Error> {
+  return useQuery(clubQueries.detail(clubId))
+}
+
+export function useClubRecruits(
+  clubId: number | string,
+): UseQueryResult<ClubRecruitsData, Error> {
+  return useQuery(clubQueries.recruit(clubId))
+}
---
+++ src/shared/providers/react-query-provider.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { PropsWithChildren, useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+export default function ReactQueryProvider({ children }: PropsWithChildren) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: 1,
+            staleTime: 1000 * 60,
+            refetchOnWindowFocus: false,
+          },
+          mutations: {
+            retry: 0,
+          },
+        },
+      }),
+  )
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {process.env.NODE_ENV === 'development' && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
+  )
+}
