4833cbfcb553e5f2b0dca9794c2de338f39ca5df
oaoong <pizzachicken@kakao.com>
2025-08-28 00:06:54 +0900

    bug: mixed-content 버그 처리

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['msw'],
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: 'upgrade-insecure-requests',
+          },
+        ],
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
